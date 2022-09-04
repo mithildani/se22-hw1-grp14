8849e89680f91652c47877b756cb4f5b945d9659
Rishikesh Pravin Yelne <ryelne@ncsu.edu>
2022-09-04 17:45:18 -0400

    Fixed compiler errors

--- app/num.py
+++ app/num.py
@@ -8,14 +8,14 @@ class Num:
         self.name = s                               # column name
         self.has = {}                               # kept data
         self.lo = sys.maxint                        # lowest seen
-        self.high = -sys.maxint - 1                 # highest seen
+        self.high = sys.minint                      # highest seen
         self.isSorted = true                        # no updates since last sort of data
-        self.w = -1 if self.s.endwith("-") else 1   # check if ending with '-', return -1 if true, 1 otherwise
+        self.w = -1 if self.s.endswith("-") else 1  # check if ending with '-', return -1 if true, 1 otherwise
     
      def nums(self):
         if (!self.isSorted):
             self.has = sorted(self.has.items(), key=lambda x: x[1])     # sort the data first
-            self.isSorted = true                                        # mark the isSorted flag true after sorting
+            self.isSorted = True                                        # mark the isSorted flag true after sorting
         return self.has
 
     
