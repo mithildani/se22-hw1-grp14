e271af2cfbf753846c34718912368f10da0f8897
Rishikesh Pravin Yelne <ryelne@ncsu.edu>
2022-09-05 20:26:50 -0400

    Fixed indentation

--- app/utilities/settings.py
+++ app/utilities/settings.py
@@ -18,39 +18,39 @@ OPTIONS:
  -S  --seperator feild seperator                       = , ]]"""
 
 def __init__(self):
-  self.help = parseInput(self.help)
-  
+	self.help = parseInput(self.help)
+
 def parseInput(self, input):
-  pattern = re.compile(r"\n [-][\S]+[\s]+[-][-]([\S]+)[^\n]+= ([\S]+)")
-  for k, x in re.findall(pattern, input):
-    self.the[k] = self.coerce(x)
-  return input
+	pattern = re.compile(r"\n [-][\S]+[\s]+[-][-]([\S]+)[^\n]+= ([\S]+)")
+	for k, x in re.findall(pattern, input):
+		self.the[k] = self.coerce(x)
+	return input
 
 def coerce(s):
-  """
-  Parse `the` config settings from `help`.
-  """
-  if isinstance(s, int):
-    return int(s)
-  return False if re.match(s, "^\s*(.-)\s*$") is None else True
+	"""
+	Parse `the` config settings from `help`.
+	"""
+	if isinstance(s, int):
+		return int(s)
+	return False if re.match(s, "^\s*(.-)\s*$") is None else True
 
 def cli(t):
-  """
-  Update settings from values on command-line flags. Booleans need no values
-  (we just flip the defaults).
-  """
-  for slot, v in t.items():
-    v = str(v)
-    for n, x in enumerate(sys.argv): 
-      if x == "-" + slot[0:1] or x == "--" + slot:
-        if v == "false":
-          v = "true"
-        elif v == "true":
-          v = "false"
-        else:
-          v = sys.argv[n + 1] 
-    t[slot] = coerce(v)
-  if t.help:
-    print("\n", t.help, "\n")
-    return
-  return t
+	"""
+	Update settings from values on command-line flags. Booleans need no values
+	(we just flip the defaults).
+	"""
+	for slot, v in t.items():
+		v = str(v)
+		for n, x in enumerate(sys.argv): 
+			if x == "-" + slot[0:1] or x == "--" + slot:
+				if v == "false":
+					v = "true"
+				elif v == "true":
+					v = "false"
+				else:
+					v = sys.argv[n + 1] 
+		t[slot] = coerce(v)
+	if t.help:
+		print("\n", t.help, "\n")
+		return
+	return t
