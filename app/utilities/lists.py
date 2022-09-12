4e9fc82d82c054c4a7a4aa924740b7ad67da4414
Neha <kaleneha997@gmail.com>
2022-09-12 16:57:02 -0400

    added push function

--- app/utilities/lists.py
+++ app/utilities/lists.py
@@ -7,23 +7,6 @@ function copy(t,    u)
   u={}; for k,v in pairs(t) do u[k] = copy(v) end
   return setmetatable(u,getmetatable(t))  end
 
--- Return the `p`-th thing from the sorted list `t`.
-function per(t,p)
-  p=math.floor(((p or .5)*#t)+.5); return t[math.max(1,math.min(#t,p))] end
-
--- Add to `t`, return `x`.
-function push(t,x) t[1+#t]=x; return x end
-
--- Call `fun` on each row. Row cells are divided in `the.seperator`.
-function csv(fname,fun,      sep,src,s,t)
-  sep = "([^" .. the.seperator .. "]+)"
-  src = io.input(fname)
-  while true do
-    s = io.read()
-    if not s then return io.close(src) else
-      t={}
-      for s1 in s:gmatch(sep) do t[1+#t] = coerce(s1) end
-      fun(t) end end end
 """
 # TODO: Write python for above code
 
@@ -47,3 +30,7 @@ def csv_func(fname, fun, n):
         for word in s[i]:
             t.append(coerce(word))
         fun(t, n)
+
+def push(t,x):
+  t.append(x)
+  return x
