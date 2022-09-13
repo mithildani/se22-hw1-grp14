7b295982a1e6c27fed97b70ff3e3b3a1ab33b81c
Rishikesh Pravin Yelne <rishikeshyelne95@gmail.com>
2022-09-13 00:52:15 -0400

    Fix Data class

--- app/objects/data.py
+++ app/objects/data.py
@@ -1,4 +1,4 @@
-from app.utilities.lists import csv, push
+from app.utilities.lists import csv_func, push
 from app.utilities.misc import obj, rnd
 from app.objects import Cols, Row, Num
 
@@ -10,36 +10,34 @@ class Data(obj):
         self.cols = None                            # summaries of data
         self.rows = []                              # kept data
         if (isinstance(src, str)):
-            self.addData(self, csv(src))
+            csv_func(src, lambda row: self.add(row))
         else:
-            src = src if src is not None else []
+            src = src if src else []
             for _, row in enumerate(src):
-                self.addData(row)
+                self.add(row)
 
-    def addData(self, data):
+    def add(self, data):
         """
         Add a row to data. Calls add() to  update the cols with new values.
         """
-        if self.cols is not None:
+        if not self.cols:
             self.cols = Cols(data)
         else:
             row = []
-            if data is not None and data.cells is not None:
+            if data is not None:
                 row = push(self.rows, data.cells)
-            elif data is not None and data.cells is None:
-                row = push(self.rows, data)
             else:
                 row = push(self.rows, Row(data))
             for _, todo in enumerate(self.cols.x, self.cols.y):
                 for _, col in enumerate(todo):
-                    Cols.addCol(row.cells[col.at])
+                    col.add(row.cells[col.at])
 
-def stats(self, showCols, func, places = 2):
-    showCols = showCols if showCols is not None else self.cols.y
-    func = func if func is not None else Num.mid
-    t = {}
-    for _, col in enumerate(showCols):
-        v = func(col)
-        v = rnd(v, places) if isinstance(v, int) else v
-        t[col.name] = v
-    return t
+    def stats(self, showCols, func, places = 2):
+        showCols = showCols if showCols is not None else self.cols.y
+        # func = func if func is not None else Num.mid
+        t = {}
+        for _, col in enumerate(showCols):
+            v = func(col)
+            v = rnd(v, places) if isinstance(v, int) else v
+            t[col.name] = v
+        return t
